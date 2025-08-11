72048f8f9f80848d56c77c06aa2c02a2d4295083
Taipoon <ca02298@cartahd.com>
2025-08-11 23:08:32 +0900

    feat: add Eggs, Lunker bass, mixed herbs.

--- Makefile
+++ Makefile
@@ -22,10 +22,21 @@ ASSETS_DIR := src/main/resources/assets
 ASSETS_TEMPLATES := src/main/templates/assets
 ITEM_NAME ?=
 
-create: _req_ITEM_NAME
+create_item: _req_ITEM_NAME
 	sed "s/###ITEM_NAME###/$(ITEM_NAME)/g" $(ASSETS_TEMPLATES)/mcre4_items.json > $(ASSETS_DIR)/mcre4/items/$(ITEM_NAME).json
 	sed "s/###ITEM_NAME###/$(ITEM_NAME)/g" $(ASSETS_TEMPLATES)/mcre4_models_item.json > $(ASSETS_DIR)/mcre4/models/item/$(ITEM_NAME).json
 	sed "s/###ITEM_NAME###/$(ITEM_NAME)/g" $(ASSETS_TEMPLATES)/mcre4_textures_item.mcmeta > $(ASSETS_DIR)/mcre4/textures/item/$(ITEM_NAME).png.mcmeta
+
+
+DATA_DIR := src/main/resources/data
+DATA_TEMPLATES := src/main/templates/data
+CATEGORY ?=
+RESULT ?=
+
+create_recipe: _req_CATEGORY _req_RESULT
+	sed "s/###CATEGORY###/$(CATEGORY)/g" $(DATA_TEMPLATES)/mcre4_recipe_shapeless.json > tmp.json
+	sed "s/###RESULT###/$(RESULT)/g" tmp.json > $(DATA_DIR)/mcre4/recipe/$(RESULT).json
+
 
 _req_%:
 	$(if $($*),,$(error variable required $*=xxx))
--- src/main/java/com/taiponappi_craft/mcre4/init/MCRE4Items.java
+++ src/main/java/com/taiponappi_craft/mcre4/init/MCRE4Items.java
@@ -5,7 +5,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -19,6 +18,15 @@ public class MCRE4Items {
     // ブラックバス
     public static final DeferredItem<Item> BLACK_BASS = ITEMS.registerSimpleItem(
         "black_bass",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(1).saturationModifier(2f).build()
+        ).stacksTo(1)
+    );
+
+    // ランカーバス
+    public static final DeferredItem<Item> LUNKER_BASS = ITEMS.registerSimpleItem(
+        "lunker_bass",
         new Item.Properties().food(
             new FoodProperties.Builder()
                 .nutrition(1).saturationModifier(2f).build()
@@ -47,16 +55,134 @@ public class MCRE4Items {
         new Item.Properties().stacksTo(1).fireResistant()
     );
 
+    // 緑+緑ハーブ
+    private static final int mixedHerbsGG = 10;
+    public static final DeferredItem<Item> MIXED_HERBS_GG = ITEMS.registerSimpleItem(
+        "mixed_herbs_gg",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(mixedHerbsGG).saturationModifier(2f).build()
+            ).stacksTo(1).fireResistant()
+    );
+
+    // 緑+赤ハーブ
+    private static final int mixedHerbsGR = 20;
+    public static final DeferredItem<Item> MIXED_HERBS_GR = ITEMS.registerSimpleItem(
+        "mixed_herbs_gr",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(mixedHerbsGR).saturationModifier(2f).build()
+            ).stacksTo(1).fireResistant()
+    );
+
+    // 緑+黄ハーブ
+    private static final int mixedHerbsGY = greenHerbNutrition;
+    public static final DeferredItem<Item> MIXED_HERBS_GY = ITEMS.registerSimpleItem(
+        "mixed_herbs_gy",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(mixedHerbsGY).saturationModifier(2f).build()
+        )
+    );
+
+    // 赤+黄ハーブ
+    public static final DeferredItem<Item> MIXED_HERBS_RY = ITEMS.registerSimpleItem(
+        "mixed_herbs_ry",
+        new Item.Properties().stacksTo(1).fireResistant()
+    );
+
+    // 緑+緑+緑ハーブ
+    private static final int mixedHerbsGGG = mixedHerbsGR;
+    public static final DeferredItem<Item> MIXED_HERBS_GGG = ITEMS.registerSimpleItem(
+        "mixed_herbs_ggg",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(mixedHerbsGGG).saturationModifier(2f).build()
+        ).stacksTo(1).fireResistant()
+    );
+
+    // 緑+赤+黄ハーブ
+    private static final int mixedHerbsGRY = mixedHerbsGR;
+    public static final DeferredItem<Item> MIXED_HERBS_GRY = ITEMS.registerSimpleItem(
+        "mixed_herbs_gry",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(mixedHerbsGRY).saturationModifier(2f).build()
+        ).stacksTo(1).fireResistant()
+    );
+
+    // 救急スプレー
+    private static final int firstAidSpray = mixedHerbsGR;
+    public static final DeferredItem<Item> FIRST_AID_SPRAY = ITEMS.registerSimpleItem(
+        "first_aid_spray",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(firstAidSpray).saturationModifier(2f).build()
+        ).stacksTo(1).fireResistant()
+    );
+
+    // 卵
+    private static final int chickenEgg = 5;
+    public static final DeferredItem<Item> CHICKEN_EGG = ITEMS.registerSimpleItem(
+        "chicken_egg",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(chickenEgg).saturationModifier(2f).build()
+        ).stacksTo(1).fireResistant()
+    );
+
+    // 卵(茶)
+    private static final int brownChickenEgg = 15;
+    public static final DeferredItem<Item> BROWN_CHICKEN_EGG = ITEMS.registerSimpleItem(
+        "brown_chicken_egg",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(brownChickenEgg).saturationModifier(2f).build()
+        ).stacksTo(1).fireResistant()
+    );
+
+    // 卵(金)
+    private static final int goldChickenEgg = mixedHerbsGR;
+    public static final DeferredItem<Item> GOLD_CHICKEN_EGG = ITEMS.registerSimpleItem(
+        "gold_chicken_egg",
+        new Item.Properties().food(
+            new FoodProperties.Builder()
+                .nutrition(goldChickenEgg).saturationModifier(2f).build()
+        ).stacksTo(1).fireResistant()
+    );
+
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> RECOVERY_ITEMS_TAB = CREATE_MODE_TABS.register(
         "recovery_items",
         () -> CreativeModeTab.builder()
-        .title(Component.translatable("itemGroup.mcre4.recovery_items")) //The language key for the title of your CreativeModeTab
+        .title(Component.translatable("itemGroup.mcre4.recovery_items"))
         .icon(() -> GREEN_HERB.get().getDefaultInstance())
         .displayItems((parameters, output) -> {
+            // ハーブ
             output.accept(GREEN_HERB.get());
             output.accept(YELLOW_HERB.get());
             output.accept(RED_HERB.get());
+
+            // ハーブ2種類組み合わせ
+            output.accept(MIXED_HERBS_GG.get());
+            output.accept(MIXED_HERBS_GR.get());
+            output.accept(MIXED_HERBS_GY.get());
+            output.accept(MIXED_HERBS_RY.get());
+
+            // ハーブ3種類組み合わせ
+            output.accept(MIXED_HERBS_GGG.get());
+            output.accept(MIXED_HERBS_GRY.get());
+
+            // 救急スプレー
+            output.accept(FIRST_AID_SPRAY.get());
+
+            // ブラックバス/ランカーバス
             output.accept(BLACK_BASS.get());
+            output.accept(LUNKER_BASS.get());
+
+            // 卵
+            output.accept(CHICKEN_EGG.get());
+            output.accept(BROWN_CHICKEN_EGG.get());
+            output.accept(GOLD_CHICKEN_EGG.get());
         }).build());
 
 
